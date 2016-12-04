898aa56f77939d463b99578c6771a5a1f61e7bac
François Laurent <francois.laurent@mail.ru>
2016-12-04 02:52:25 +0100

    cleaning

--- spatial/graph/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from .exception import GraphError, AbstractGraphError, NodeError, EdgeError, \
-	MissingNodeError, MissingEdgeError, NodeAttributeError, EdgeAttributeError
-from .base import Graph
-
-__all__ = ['GraphError', 'AbstractGraphError', 'NodeError', 'EdgeError', \
-	'MissingNodeError', 'MissingEdgeError', 'NodeAttributeError', 'EdgeAttributeError', 'Graph']
-
--- tesselation/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from .base import *
-from .kmeans import *
-from .gas import *
-
-__all__ = ['CellStats', 'Tesselation', 'Voronoi', 'KMeansMesh', 'GasMesh']
-
