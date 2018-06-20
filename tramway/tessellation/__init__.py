07e77d0b4dabc680e4d6c0b4ba2fb19539234f18
François Laurent <francois.laurent@mail.ru>
2018-06-20 18:45:22 +0200

    abnormally missing init file restored

---
+++ tramway/tessellation/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Copyright © 2017, Institut Pasteur
+#   Contributor: François Laurent
+
+# This file is part of the TRamWAy software available at
+# "https://github.com/DecBayComp/TRamWAy" and is distributed under
+# the terms of the CeCILL license as circulated at the following URL
+# "http://www.cecill.info/licenses.en.html".
+
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license and that you accept its terms.
+
+
+from .base import *
+from .time import *
+from .nesting import *
+from tramway.core.plugin import list_plugins
+import os.path
+
+
+try:
+        from .grid import RegularMesh
+except ImportError:
+        pass
+
+
+plugins = list_plugins(os.path.dirname(__file__), __package__, \
+        {'make': Tessellation}, require='setup')
+
