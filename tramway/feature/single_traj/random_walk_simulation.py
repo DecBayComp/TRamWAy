cec8aa952b1ec16a90e0a04c6d3811486e1491ab
mDuval1 <maximeduvalsy@gmail.com>
2019-03-20 10:48:48 +0100

    Header commit

---
+++ tramway/feature/single_traj/random_walk_simulation.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+# Copyright © 2017-2019, Institut Pasteur
+#    Contributor: Maxime Duval
+
+# This file is part of the TRamWAy software available at
+# "https://github.com/DecBayComp/TRamWAy" and is distributed under
+# the terms of the CeCILL license as circulated at the following URL
+# "http://www.cecill.info/licenses.en.html".
+
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license and that you accept its terms.
+
+"""
+This file regroups random walk simulations of many different kinds.
+"""
+
