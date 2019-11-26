1c0d62d93c0babf8df03db87f78d976a6d13a6d7
jbmasson <jean-baptiste.masson@pasteur.fr>
2019-11-26 10:58:15 +0100

    test

--- notes/To-Do.sh
+++ notes/To-Do.sh
@@ -1,3 +1,4 @@
+- modify loading module for init files
 - implement non tracking at temperature = 0 
 - implement sampling of assigmenets for mixed density cases
 - regenerate trajectories from assigments 
