7a8132520635f2bf5ffab821b4349f1078a92ecd
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-10 15:00:46 +0000

    Adding tarfile member sanitization to extractall()

--- notebooks/basic/tutorial.py
+++ notebooks/basic/tutorial.py
@@ -163,7 +163,26 @@ def download_non_tracking_test_data():
         urlretrieve(data_archive, test_data_dir+'.tar.bz2')
         import tarfile
         with tarfile.open(test_data_dir+'.tar.bz2') as archive:
-            archive.extractall(os.path.dirname(test_data_dir))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, os.path.dirname(test_data_dir))
 
 
 __all__ = ['trajectory_file', 'rwa_file',
--- notebooks/tutorial.py
+++ notebooks/tutorial.py
@@ -34,7 +34,26 @@ def download_RWAnalyzer_tour_data():
         urlretrieve(data_archive, tutorial_data_file)
         import tarfile
         with tarfile.open(tutorial_data_file) as archive:
-            archive.extractall(os.path.dirname(tutorial_data_dir))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, os.path.dirname(tutorial_data_dir))
 
 def print_analysis_tree(analyses, annotations=False, **kwargs):
     """
